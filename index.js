5d544f0810b3e1a9619a4d502bd7b02040b8a2a1
Pedro Coutinho de Freitas <pedro.coutinhof001@gmail.com>
2026-03-09 21:18:41 -0300

    Remove emojis from console log messages

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 // Configuração para aceitar JSON
 app.use(express.json());
 
-console.log("⚠️ ATENÇÃO: API rodando em Modo de Persistência em Memória");
+console.log(" ATENÇÃO: API rodando em Modo de Persistência em Memória");
 
 // Rotas (Endpoints)
 app.post('/order', orderController.createOrder);
@@ -14,5 +14,5 @@ app.get('/order/:id', orderController.getOrder);
 
 // Liga o servidor
 app.listen(3000, () => {
-    console.log("🚀 API ONLINE na porta 3000! Sem erros de conexão.");
+    console.log(" API ONLINE na porta 3000! Sem erros de conexão.");
 });
